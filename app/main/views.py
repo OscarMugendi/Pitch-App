ba9e929a0bf8567c28812dd893b590f7b032e528
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-17 14:21:13 +0300

    Basic app user structure

--- app/main/views.py
+++ app/main/views.py
@@ -10,3 +10,12 @@ from ..models import User
 def index():
     #posts = Pitch.query.all()
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile.html", user = user)
